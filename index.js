376ca45d21f8792ba77be524f4f8f142a8428a8b
zorgonide <48021258+zorgonide@users.noreply.github.com>
2023-05-24 12:21:11 +0100

    2 files modified

--- index.js
+++ index.js
@@ -507,3 +507,50 @@ const root = createTrie(CITY_NAMES.slice(0, 10));
 // console.log(printTrie(root));
 console.log(root.complete('ph'));
 console.log('--------------Tries end----------------');
+console.log('--------------Bloom filters begin----------------');
+const XXH = require('xxhashjs');
+const h1 = (string) =>
+  Math.abs(XXH.h32(0xabcd).update(string).digest().toNumber() % 100);
+const h2 = (string) =>
+  Math.abs(XXH.h32(0x1234).update(string).digest().toNumber() % 100);
+const h3 = (string) =>
+  Math.abs(XXH.h32(0x6789).update(string).digest().toNumber() % 100);
+
+//generate hash between 0-100
+class BloomFilter {
+  #array;
+  constructor() {
+    this.#array = new Array(100).fill(0);
+  }
+  add(string) {
+    this.#array[h1(string)] = 1;
+    this.#array[h2(string)] = 1;
+    this.#array[h3(string)] = 1;
+  }
+  contains(string) {
+    return !!(
+      this.#array[h3(string)] &&
+      this.#array[h2(string)] &&
+      this.#array[h1(string)]
+    );
+  }
+}
+bf = new BloomFilter();
+// console.log(bf.contains('Brian'));
+const names = [
+  'Brian',
+  'Simona',
+  'Sarah',
+  'Asim',
+  'John',
+  'Sean',
+  'Jessie',
+  'Paige',
+  'Ashley',
+];
+names.forEach((item) => bf.add(item));
+['Sam', 'Brian', 'Taylor', 'Florence', 'Asim'].forEach((item) =>
+  console.log(bf.contains(item))
+);
+
+console.log('--------------Bloom filters end----------------');
