4b702d3b85c270e3b262e4b203e0ddc5fca8184b
zorgonide <48021258+zorgonide@users.noreply.github.com>
2023-05-25 17:01:03 +0100

    1 file modified

--- index.js
+++ index.js
@@ -113,6 +113,103 @@ class Node {
   }
 }
 
+console.log('---------Stack start---------');
+class Stack {
+  #storage;
+  constructor() {
+    this.#storage = {};
+    this.length = 0;
+  }
+  push(value) {
+    this.#storage[this.length++] = value;
+  }
+  pop() {
+    if (this.length > 0) {
+      const value = this.#storage[this.length - 1];
+      delete this.#storage[this.length - 1];
+      this.length--;
+      return value;
+    } else return undefined;
+  }
+  peek() {
+    return this.#storage[this.length - 1];
+  }
+}
+let s = new Stack();
+s.push(1);
+s.push(2);
+s.push(3);
+s.push(4);
+s.push(5);
+// console.log(s);
+// console.log(s.peek());
+// console.log(s.pop());
+// console.log(s.length);
+// console.log(s.pop());
+// console.log(s.pop());
+// console.log(s.pop());
+// console.log(s.pop());
+// console.log(s.pop()); // undefined
+// console.log(s.pop()); // undefined
+// console.log(s.pop()); // undefined
+// console.log(s.length); // undefined
+
+console.log('----------stack end--------------');
+console.log('----------queue start--------------');
+
+class Queue {
+  #storage;
+  constructor() {
+    this.#storage = {};
+    this.length = 0;
+    this.head = 0;
+  }
+  enqueue(value) {
+    this.#storage[this.length++ + this.head] = value;
+  }
+  dequeue() {
+    if (this.length) {
+      const value = this.#storage[this.head];
+      delete this.#storage[this.head];
+      this.length--;
+      this.head++;
+      return value;
+    } else {
+      this.head = 0;
+      return undefined;
+    }
+  }
+  peek() {
+    return this.#storage[this.head];
+  }
+}
+let q = new Queue();
+q.enqueue(1);
+q.enqueue(2);
+q.enqueue(3);
+q.enqueue(4);
+q.enqueue(5);
+console.log(q);
+// console.log(q.peek());
+console.log(q.dequeue());
+// console.log(q.length);
+console.log(q.dequeue());
+console.log(q.dequeue());
+console.log(q.dequeue());
+console.log('should be 1 =>' + q.length); // undefined
+q.enqueue(1);
+q.enqueue(2);
+q.enqueue(3);
+q.enqueue(4);
+console.log(q.dequeue());
+console.log(q.dequeue());
+console.log(q.dequeue());
+console.log(q.dequeue());
+console.log(q.dequeue());
+console.log('should be undefined =>' + q.dequeue()); // undefined
+
+console.log('----------queue end--------------');
+console.log('----------Linkedlist start--------------');
 class LinkedList {
   constructor() {
     this.head = null;
