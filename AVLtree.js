a3d2c1e93c9b8b1b88d46a824d42e9697c99cf7c
zorgonide <48021258+zorgonide@users.noreply.github.com>
2023-06-11 14:31:09 +0100

    2 files modified

--- AVLtree.js
+++ AVLtree.js
@@ -1,61 +1,64 @@
 //AVL TREE
 
-class AVLTree {
-  // code goes here
+class Tree {
   constructor() {
     this.root = null;
   }
   add(value) {
     if (!this.root) {
-      this.root = new AVLNp(value);
-    } else {
-      this.root.add(value);
-    }
+      this.root = new Node(value);
+    } else this.root.add(value);
   }
   toObject() {
     return this.root;
   }
 }
 
-class AVLNode {
-  // code also goes here
+class Node {
   constructor(value) {
-    this.left = null;
-    this.right = null;
     this.value = value;
     this.height = 1;
+    this.right = null;
+    this.left = null;
   }
   add(value) {
     if (value < this.value) {
-      // go left
-      if (this.left) this.left.add(value);
-      else {
-        this.left = new AVLNode(value);
-      }
-      this.updateInNewLocation();
+      if (this.left) {
+        // go left
+        this.left.add(value);
+      } else this.left = new Node(value);
+      this.updateHeightInLocation();
     } else {
       // go right
       if (this.right) this.right.add(value);
-      else this.right = new AVLNode(value);
-      this.updateInNewLocation();
+      else this.right = new Node(value);
+      this.updateHeightInLocation();
     }
     this.balance();
   }
+  updateHeightInLocation() {
+    if (!this.left && !this.right) {
+      this.height = 1;
+    } else if (!this.right || this.right.height < this.left.height) {
+      this.height = this.left.height + 1;
+    } else if (!this.left || this.left.height < this.right.height) {
+      this.height = this.right.height + 1;
+    }
+  }
   balance() {
-    let rightHeight = this.right?.height || 0;
     let leftHeight = this.left?.height || 0;
-
+    let rightHeight = this.right?.height || 0;
     if (leftHeight > rightHeight + 1) {
-      let leftLeftHeight = this.left?.left?.height || 0;
-      let leftRightHeight = this.left?.right?.height || 0;
+      let leftLeftHeight = this.left.left?.height; // check if more ? needed
+      let leftRightHeight = this.left.right?.height;
 
       if (leftRightHeight > leftLeftHeight) {
         this.left.rotateRR();
       }
       this.rotateLL();
     } else if (rightHeight > leftHeight + 1) {
-      let rightLeftHeight = this.right?.left?.height || 0;
-      let rightRightHeight = this.right?.right?.height || 0;
+      let rightRightHeight = this.right.right?.height;
+      let rightLeftHeight = this.right.left?.height;
 
       if (rightLeftHeight > rightRightHeight) {
         this.right.rotateLL();
@@ -68,37 +71,68 @@ class AVLNode {
     let leftBefore = this.left;
     this.value = this.right.value;
     this.left = this.right;
+    this.left.right = this.right.left;
     this.right = this.right.right;
-    this.left.right = this.left.left;
     this.left.left = leftBefore;
     this.left.value = valueBefore;
-    this.left.updateInNewLocation();
-    this.updateInNewLocation();
+    this.left.updateHeightInLocation();
+    this.updateHeightInLocation();
   }
-  rotateLL() {
+  rotateRR() {
     let valueBefore = this.value;
     let rightBefore = this.right;
     this.value = this.left.value;
     this.right = this.left;
+    this.right.left = this.left.right;
     this.left = this.left.left;
-    this.right.left = this.right.right;
     this.right.right = rightBefore;
     this.right.value = valueBefore;
-    this.right.updateInNewLocation();
-    this.updateInNewLocation();
-  }
-  updateInNewLocation() {
-    if (!this.right && !this.left) this.height = 1;
-    else if (!this.right || this.left?.height > this.right?.height)
-      this.height = this.left.height + 1;
-    else if (!this.left || this.right?.height > this.left?.height)
-      this.height = this.right.height + 1;
+    this.right.updateHeightInLocation();
+    this.updateHeightInLocation();
   }
 }
 
+const tree = new Tree();
+// Add values to the tree
+tree.add(10);
+tree.add(5);
+tree.add(15);
+tree.add(3);
+tree.add(8);
+
+// Print the tree structure
+console.log(tree.toObject());
+// Expected output:
+// {
+//   value: 10,
+//   left: {
+//     value: 5,
+//     left: {
+//       value: 3,
+//       left: null,
+//       right: null,
+//       height: 1
+//     },
+//     right: {
+//       value: 8,
+//       left: null,
+//       right: null,
+//       height: 1
+//     },
+//     height: 2
+//   },
+//   right: {
+//     value: 15,
+//     left: null,
+//     right: null,
+//     height: 1
+//   },
+//   height: 3
+// }
 console.log('----------AVL END-------------');
 
 /**
+ * updateInNewLocation
  * If the node has no left child and no right child (i.e., it is a leaf node), then its height is set to 1. This is because a leaf node is considered to have a height of 1.
 
 If the node has no right child or its left child's height is greater than its right child's height, then its height is set to the height of its left child plus 1. This is because the node's height is determined by the height of its left subtree.
